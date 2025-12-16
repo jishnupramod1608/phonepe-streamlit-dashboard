bd3eb992ec8b0aa552c8f69e0fa97a83b1cda1b6
jishnupramod1608 <jishnu.pramod2426p@ves.ac.in>
2025-12-16 22:17:56 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -253,10 +253,6 @@ st.sidebar.header("Controls")
 uploaded_file = st.sidebar.file_uploader(
     "Upload your transactions (.xlsx or .csv)", type=["xlsx", "csv"]
 )
-st.sidebar.markdown("---")
-
-if st.sidebar.button("Go to Upload / Data Page"):
-    st.switch_page("pages/upload_data.py")
 raw_df = load_uploaded(uploaded_file)
 
 if raw_df is None:
@@ -661,6 +657,7 @@ else:
 # End
 
 # ---------------------------
+
 
 
 
