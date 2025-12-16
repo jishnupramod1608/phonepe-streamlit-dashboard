91aabd2472b3db53c81bad8a124a1fe0275932c7
jishnupramod1608 <jishnu.pramod2426p@ves.ac.in>
2025-12-16 21:48:35 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -255,7 +255,7 @@ uploaded_file = st.sidebar.file_uploader(
 )
 st.sidebar.markdown("---")
 if st.sidebar.button("Go to Upload / Data Page"):
-    st.switch_page("pages/upload_data.py")
+    st.switch_page("upload_data")
 raw_df = load_uploaded(uploaded_file)
 
 if raw_df is None:
@@ -660,6 +660,7 @@ else:
 # End
 
 # ---------------------------
+
 
 
 
