b7964d4453f7810a5c15ce7c228207168c439843
jishnupramod1608 <jishnu.pramod2426p@ves.ac.in>
2025-12-16 16:03:48 +0530

    upload_data.py

---
+++ pages/upload_data.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Upload Data", layout="wide")
+
+st.title("Upload & Preprocess Data")
+
+st.write("This page is for uploading and preparing transaction data.")
